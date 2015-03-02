eee98455d2a029742179617382ca2d0ff65d6ba0
Martin Vieth <martin_123_vieth@hotmail.com>
2015-03-02 15:19:21 +0100

    123

---
+++ Lektion5/src/opgaver/ReadDir.java
@@ -0,0 +1,21 @@
+package opgaver;
+
+import java.io.File;
+
+public class ReadDir {
+
+	public static void main(String[] args){
+
+		File file = null;
+		String[] paths;
+		
+		try{
+			//Create new file object
+			file = new File(clausstaffe/);
+			paths.file
+		}catch(){
+			
+		}
+		
+	}
+}
